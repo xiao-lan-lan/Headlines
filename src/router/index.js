d6be6bc12ad56e79f8a4172226595ed5c63586bb
xiao-lan-lan <479323417@qq.com>
2019-11-13 18:03:03 +0800

    配置-一级路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 引入一级路由模块
+import login from '../views/login.vue'
+import home from '../views/home.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
+  // 单页应用：一级路由就是登录页和主页;
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: login
+  },
+  {
+    path: '/home',
+    component: home
+  }
+
   // {
   //   path: '/about',
   //   name: 'about',
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view></router-view>
+  <div>主页</div>
 </template>
 
 <script>
