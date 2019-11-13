aad79f3fc5bf0128674dc62d882bfdbe0bd95dd1
xiao-lan-lan <479323417@qq.com>
2019-11-13 21:11:36 +0800

    login-结构&样式

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import './assets/css/index.less'
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,57 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+    <!-- 卡片div -->
+    <el-card class="login-card">
+      <!-- logo图片 -->
+      <div class="logo-img">
+        <img src="../assets/img/logo_index.png" alt />
+      </div>
+      <!-- 表单 -->
+      <el-form class="demo-ruleForm" style="margin-top:18px">
+        <!-- 表单每一项 -->
+        <!-- 手机号 -->
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item style="margin-bottom:10px">
+          <el-input placeholder="验证码" style="width:220px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <!-- 复选框 -->
+        <el-form-item style="margin-bottom:10px">
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+       </el-form-item>
+       <!-- 按钮 -->
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped >
+.login {
+  background-image: url("../assets/img/login_bg.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 380px;
+    height: 310px;
+    .logo-img {
+      text-align: center;
+      img {
+        height: 34px;
+      }
+    }
+  }
+}
 </style>
