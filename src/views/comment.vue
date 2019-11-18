c818ba502911f522f3a20bc11f5d22d937dc0b30
兰小兰 <479323417@qq.com>
2019-11-18 17:44:14 +0800

    comment-修改文章评论状态

--- src/views/comment.vue
+++ src/views/comment.vue
@@ -4,17 +4,28 @@
       <!-- 评论数据表格 -->
       <el-table :data="commentData" border style="width: 100%">
         <el-table-column prop="title" label="标题" width="350"></el-table-column>
-        <el-table-column prop="comment_status" label="评论状态" align="center">
-          <template slot-scope="scope">
-            <el-tag v-if="scope.row.comment_status" type="success">正常</el-tag>
-            <el-tag v-else type="info">关闭</el-tag>
-          </template>
-        </el-table-column>
+
         <el-table-column prop="total_comment_count" label="总评论数" width="150" align="center"></el-table-column>
         <el-table-column prop="fans_comment_count" label="粉丝评论数" width="150" align="center"></el-table-column>
+
+        <el-table-column label="评论状态" align="center">
+          <!-- <template slot-scope="scope">
+            <el-tag v-if="scope.row.comment_status" type="success">正常</el-tag>
+            <el-tag v-else type="info">关闭</el-tag>
+          </template>-->
+
+         <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#409eff"
+              inactive-color="#dcdfe6"
+              @change="onStatusChange(scope.row)"
+            ></el-switch>
+         </template>
+        </el-table-column>
+
         <el-table-column label="操作" align="center">
           <template>
-            <el-link type="primary" class="edit">修改</el-link>
             <el-link type="danger">关闭评论</el-link>
           </template>
         </el-table-column>
@@ -28,7 +39,6 @@
         layout="total, prev, pager, next, jumper"
         :total="total_count"
       ></el-pagination>
-
     </el-card>
   </div>
 </template>
@@ -39,8 +49,7 @@ export default {
   data () {
     return {
       commentData: [],
-      comment_status: true,
-      total_count: '',
+      total_count: 0,
       current_page: ''
     }
   },
@@ -69,9 +78,35 @@ export default {
         })
     },
 
+    // 分页切换
     handleCurrentChange (page) {
       console.log(page)
       this.loadComments(page)
+    },
+
+    // 切换评论状态
+    onStatusChange (item) {
+      console.log(item)
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: item.id.toString()
+        },
+        data: {
+          allow_comment: item.comment_status
+        }
+      }).then(res => {
+        console.log(res.data)
+        this.$message({
+          message: `恭喜你，${item.comment_status ? '开启' : '关闭'}成功`,
+          type: 'success',
+          duration: 1000
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('错了哦，错误错误')
+      })
     }
   },
   created () {
