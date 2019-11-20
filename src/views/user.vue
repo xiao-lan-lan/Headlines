00fe4012113895baab03b5c3a1565f3b79d06b44
兰小兰 <479323417@qq.com>
2019-11-20 22:58:32 +0800

    user-修改姓名邮箱弹框

--- src/views/user.vue
+++ src/views/user.vue
@@ -5,17 +5,45 @@
         <span>账号信息</span>
       </div>
       <el-form ref="form" :model="userForm" label-width="80px">
+        <!-- 头像 -->
         <div class="username el-form-item">
-            <div class="left" style="float:left">
-                <img :src="userForm.photo" width="60px" height="60px" style="display:block;border-radius: 50%;margin-bottom:5px">
-                <el-link type="primary">更换头像</el-link>
-            </div>
-          <div class="rightcontent" style="float:left">
+          <div class="left" style="float:left">
+            <img
+              :src="userForm.photo"
+              width="60px"
+              height="60px"
+              style="display:block;border-radius: 50%;margin-bottom:5px"
+            />
+            <el-link type="primary">更换头像</el-link>
+          </div>
+          <!-- 姓名 -->
+          <div class="rightcontent" style="float:left" v-if="editname">
             <h3>{{userForm.name}}</h3>
             <span style="margin-left:0;font-size:12px">{{userForm.intro}}</span>
           </div>
-          <el-button type="primary" style="float:right">修改</el-button>
+
+          <!-- 编辑状态 -->
+          <div class="editname" v-else>
+            <el-form-item label="名称">
+              <el-input v-model="userForm.name" style="width:300px"></el-input>
+            </el-form-item>
+            <el-form-item label="简介" style="padding-left:60px">
+              <el-input v-model="userForm.intro" style="width:300px"></el-input>
+            </el-form-item>
+            <el-form-item style="padding-left:60px;margin-top:50px">
+              <el-button type="primary">保存</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </div>
+          <el-button
+            type="primary"
+            style="float:right"
+            @click="editname=!editname"
+            v-show="editname"
+          >修改</el-button>
         </div>
+
+        <!-- 账号信息 -->
         <el-form-item label="账号信息" class="userinfo">
           <div class="rightcontent">
             <div class="type">
@@ -29,6 +57,7 @@
           </div>
         </el-form-item>
 
+        <!-- 登录方式 -->
         <el-form-item label="登录方式" class="userinfo">
           <div class="rightcontent">
             <div>
@@ -37,10 +66,22 @@
             </div>
           </div>
         </el-form-item>
+
+        <!-- 邮箱 -->
         <el-form-item label="邮箱" class="userinfo">
-          <div class="rightcontent">
+          <div class="rightcontent" v-if="editemail">
             <span style="margin-left:0">{{userForm.email}}</span>
-            <el-button type="primary" style="float:right">修改邮箱</el-button>
+            <el-button type="primary" style="float:right" v-show="editemail" @click="editemail=!editemail">修改邮箱</el-button>
+          </div>
+          <!-- 编辑状态 -->
+          <div class="editemail" style="margin-left:30px" v-else>
+            <el-form-item>
+              <el-input v-model="userForm.email" style="width:300px"></el-input>
+            </el-form-item>
+            <el-form-item style="margin-top:20px">
+              <el-button type="primary">保存</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
           </div>
         </el-form-item>
       </el-form>
@@ -58,7 +99,9 @@ export default {
         name: '',
         intro: '',
         photo: ''
-      }
+      },
+      editname: true,
+      editemail: true
     }
   },
   methods: {
@@ -85,19 +128,25 @@ export default {
 
 <style lang='less' scoped>
 .user {
-  .userinfo,.username {
+  .username {
+    margin-left: 10px;
+    .rightcontent {
+      margin-left: 40px;
+    }
+  }
+  .userinfo {
     margin-top: 25px;
     border-top: 1px solid #ccc;
     padding-top: 25px;
     .rightcontent {
       margin-left: 30px;
       h3 {
-          margin: 0;
+        margin: 0;
       }
       .type {
-          border-bottom: 1px solid #ccc;
-          margin-bottom: 15px;
-          padding-bottom: 15px;
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 15px;
+        padding-bottom: 15px;
       }
       span {
         margin-left: 30px;
