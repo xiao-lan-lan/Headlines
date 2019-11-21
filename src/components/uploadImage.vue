c858c81403966114978c29128f061dcf680b69e9
兰小兰 <479323417@qq.com>
2019-11-21 22:31:56 +0800

    uploadImage-弹出对话框基本结构

--- src/components/uploadImage.vue
+++ src/components/uploadImage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="uploadImage">
-    <div class="block">
+    <div class="block" @click="dialogTableVisible=true">
       <span class="demonstration">点击图标选择图片</span>
       <br />
       <el-image style="margin-top:60px">
@@ -10,6 +10,25 @@
       </el-image>
     </div>
 
+    <!-- 对话框 -->
+    <el-dialog :visible.sync="dialogTableVisible">
+      <!-- tab标签 -->
+      <el-tabs type="border-card">
+        <el-tab-pane label="素材库">
+
+          <!-- 单选按钮组 -->
+          <el-radio-group v-model="radio1">
+            <el-radio-button label="全部"></el-radio-button>
+            <el-radio-button label="收藏"></el-radio-button>
+          </el-radio-group>
+        </el-tab-pane>
+        <el-tab-pane label="上传图片"></el-tab-pane>
+
+        <!-- button组 -->
+        <el-button type="primary">确定</el-button>
+        <el-button @click="dialogTableVisible=false">取消</el-button>
+      </el-tabs>
+    </el-dialog>
   </div>
 </template>
 
@@ -17,7 +36,8 @@
 export default {
   data () {
     return {
-      dialogTableVisible: false
+      dialogTableVisible: false,
+      radio1: '全部'
     }
   }
 }
