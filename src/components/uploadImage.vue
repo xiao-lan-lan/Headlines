bcbc1856f3c4fab9985e17a658044ff5e0c673d6
兰小兰 <479323417@qq.com>
2019-11-22 23:59:54 +0800

    uploadImage-上传图片预览与请求

--- src/components/uploadImage.vue
+++ src/components/uploadImage.vue
@@ -2,7 +2,7 @@
   <div class="uploadImage">
     <div class="block" @click="uploadimage">
       <span class="demonstration">点击图标选择图片</span>
-      <img v-if="this.value" :src="this.value" class="avatar" width="180px" height="150px"/>
+      <img v-if="this.value" :src="this.value" class="avatar" width="180px" height="150px" />
       <i v-else class="el-icon-plus avatar-uploader-icon"></i>
     </div>
 
@@ -31,23 +31,47 @@
               <i :class="{'el-icon-check':true, 'img-item':image==Imgitem}"></i>
             </el-col>
           </el-row>
+
+          <!-- 分页器 -->
+          <el-pagination
+            @current-change="onPageChange"
+            @size-change="onSizeChange"
+            :current-page="1"
+            :page-sizes="[20, 40, 60]"
+            :page-size="20"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total_count"
+            style="text-align:center"
+            background
+          ></el-pagination>
         </el-tab-pane>
 
         <!-- 上传图片 -->
-        <el-tab-pane label="上传图片"></el-tab-pane>
-
-        <!-- 分页器 -->
-        <el-pagination
-          @current-change="onPageChange"
-          @size-change="onSizeChange"
-          :current-page="1"
-          :page-sizes="[20, 40, 60]"
-          :page-size="20"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="total_count"
-          style="text-align:center"
-          background
-        ></el-pagination>
+        <el-tab-pane label="上传图片">
+          <div class="block">
+            <span class="demonstration">点击图标选择图片</span>
+            <img v-if="PreviewImg" :src="PreviewImg" class="avatar" width="180px" height="150px" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </div>
+          <el-upload
+            class="upload-demo"
+            ref="upload"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            :headers="uploadImgHeader"
+            name='image'
+            :file-list="fileList"
+            :auto-upload="false"
+            :on-success="onPreviewImg"
+          >
+            <el-button slot="trigger" size="small" type="primary">点击选择图片</el-button>
+            <el-button
+              style="margin-left: 10px;"
+              size="small"
+              type="success"
+              @click="submitUpload"
+            >开始上传</el-button>
+          </el-upload>
+        </el-tab-pane>
 
         <!-- button组 -->
         <el-button type="primary" @click="onPreview">确定</el-button>
@@ -66,7 +90,12 @@ export default {
       radio1: '全部',
       total_count: 0,
       images: [],
-      Imgitem: ''
+      Imgitem: '',
+      fileList: [],
+      uploadImgHeader: {
+        Authorization: `Bearer ${localStorage.getItem('user')}`
+      },
+      PreviewImg: ''
     }
   },
   props: {
@@ -113,10 +142,21 @@ export default {
       this.loadMaterial(1, pageSize)
     },
 
-    // 预览图片
+    // 素材库，预览图片
     onPreview () {
       this.dialogTableVisible = false
       this.$emit('input', this.Imgitem.url)
+    },
+
+    // 上传图片，点击发送请求
+    submitUpload () {
+      this.$refs.upload.submit()
+    },
+
+    // 上传图片，预览
+    onPreviewImg (res, b, c) {
+      console.log(res, b, c)
+      this.PreviewImg = res.data.url
     }
   }
 }
